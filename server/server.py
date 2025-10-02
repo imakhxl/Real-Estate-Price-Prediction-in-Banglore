c752cedb2b6e87b5346d3adc2d1fad460a93163c
Akhil J <akhilj2003@gmail.com>
2025-10-02 20:10:37 +0530

    Added flask server file and tested http endpoints using Postman

--- server/server.py
+++ server/server.py
@@ -1,6 +1,52 @@
 from flask import Flask, request, jsonify
+import util
+
 app = Flask(__name__)
+
+@app.route('/get_location_names', methods=['GET'])
+def get_location_names():
+    response = jsonify({
+        'locations': util.get_location_names()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+@app.route('/predict_home_price', methods=['GET', 'POST'])
+def predict_home_price():
+    total_sqft = float(request.form['total_sqft'])
+    location = request.form['location']
+    bhk = int(request.form['bhk'])
+    bath = int(request.form['bath'])
+
+    response = jsonify({
+        'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+# @app.route('/predict_home_price', methods=['GET', 'POST'])
+# def predict_home_price():
+#     data = request.get_json()
+#     print("DEBUG incoming data:", data)  # 👈 print incoming request
+#     if not data:
+#         return jsonify({"error": "No JSON received"}), 400
+#
+#     total_sqft = float(data['total_sqft'])
+#     location = data['location']
+#     bhk = int(data['bhk'])
+#     bath = int(data['bath'])
+#
+#     price = util.get_estimated_price(location, total_sqft, bhk, bath)
+#     print("DEBUG predicted price:", price)  # 👈 log prediction
+#
+#     response = jsonify({'estimated_price': price})
+#     response.headers.add('Access-Control-Allow-Origin', '*')
+#     return response
 
 
 if __name__ == "__main__":
-    print("Startingf")
+    print("Starting Python Flask Server For Home Price Prediction...")
+    util.load_saved_artifacts()
+    app.run()
